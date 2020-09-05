31b7e5ba809cd496e67d73b8159024380e286a56
jorgeemzu <jorge.emzu@gmail.com>
2020-09-05 10:40:47 -0430

    se agrego lazy loading usando intersection observers de javascript

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menu');
 const hamburger = document.querySelector('.hamburger');
+const fadeIn = document.querySelectorAll('.fadein');
 
 hamburger.addEventListener('click', showMenu);
 
@@ -7,3 +8,23 @@ function showMenu() {
 	menu.classList.toggle('showMenu');
 	hamburger.classList.toggle('rotate_hamburger');
 }
+
+const options = {
+	threshold: 0.3,
+	rootMargin: '0px 0px -20px 0px',
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			return;
+		} else {
+			entry.target.classList.add('apear');
+			appearOnScroll.unobserve(entry.target);
+		}
+	});
+}, options);
+
+fadeIn.forEach(fader => {
+	appearOnScroll.observe(fader);
+});
